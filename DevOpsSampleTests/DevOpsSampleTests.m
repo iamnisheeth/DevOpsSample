b441a13199b8f8f35ed36c1390f04dc0dbe96306
iamnisheeth <iamnisheeth@gmail.com>
2014-12-04 17:50:39 +0530

    Updated BundleID
    Added updated XcodeProj with new BundleID

---
+++ DevOpsSampleTests/DevOpsSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  DevOpsSampleTests.m
+//  DevOpsSampleTests
+//
+//  Created by Nisheeth on 12/4/14.
+//  Copyright (c) 2014 Nisheeth. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DevOpsSampleTests : XCTestCase
+
+@end
+
+@implementation DevOpsSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
