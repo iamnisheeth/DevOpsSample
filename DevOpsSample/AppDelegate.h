7dcb64f5505e01eda5cc64482d5004a5741a0201
iamnisheeth <iamnisheeth@gmail.com>
2014-12-04 15:14:56 +0530

    Cloned local XcodeProj to remote repository
    Cloned local Xcode.proj

---
+++ DevOpsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DevOpsSample
+//
+//  Created by Nisheeth on 12/4/14.
+//  Copyright (c) 2014 Nisheeth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
